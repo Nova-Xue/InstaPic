89b86233b069b30b2af5200bdf7fe77cdd2de412
Nova <xueshengyuan1118@hotmail.com>
2019-11-08 18:30:18 -0500

    Show post view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    show post {{$post->id}}
+<div class="container d-flex">
+    <div class="row">
+        <div class="col-md-8">
+            <img src="/storage/{{$post->image}}" alt="post" class="w-100">
+        </div>
+        <div class="col-md-4">
+            <div>{{$post->caption}}</div>
+        </div>
+    </div>
 </div>
 @endsection
